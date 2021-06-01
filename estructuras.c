5232b81091c8085abbc5a36f3238aa43e21977b3
Arkanyan <Arkanyan@users.noreply.github.com>
2021-06-01 20:24:04 -0300

    Estructuras Basicas

---
+++ estructuras.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct estructuraNodo {
+    int valor;
+    struct estructuraNodo *proximo;
+    
+} Nodo;
+
+void agregarElemento(Nodo *lista, int valor) {
+    Nodo *nodoNuevo = malloc(sizeof(Nodo));
+    nodoNuevo->valor = valor;
+    nodoNuevo->proximo = NULL;
+    if (lista == NULL) {
+        lista = nodoNuevo;
+    } else {
+        Nodo *cursor = lista;
+        while (cursor->proximo != NULL) {
+            cursor = cursor->proximo;
+        }
+        cursor->proximo = nodoNuevo;
+    }
+}
+
+Nodo *crearLista() {
+    Nodo *lista = malloc(sizeof(Nodo));
+    lista = NULL;
+    return lista;
+}
+
+int largoDeLista(Nodo *lista){
+    if (lista == NULL) {
+       return 0;
+    } else {
+        int contador = 0;
+        Nodo *cursor = lista;
+        while (cursor != NULL) {
+            contador = contador+1;
+            cursor = cursor->proximo;
+        }
+        return contador;
+    }
+    return 0;
+}
+
+Nodo obtenerNodo (Nodo *lista, int posicion){
+    Nodo nodoObtenido;
+    if(lista == NULL || posicion >= largoDeLista(lista)){
+        return nodoObtenido;
+    } else{
+        Nodo *cursor = lista;
+        for(int i = 0; i < posicion ;i =i+1){
+            cursor = cursor->proximo;
+        }
+        nodoObtenido.valor = cursor->valor;
+        return nodoObtenido;
+    }
+}
+
+void eliminarNodo (Nodo *lista, int posicion){
+    if(lista == NULL || posicion >= largoDeLista(lista)){
+        return;
+    } else{
+        Nodo *cursor = lista;
+        for(int i = 0; i < posicion-1 ;i =i+1){
+            cursor = cursor->proximo;
+        }
+        Nodo *proximo = cursor->proximo->proximo;
+        free(cursor->proximo);
+        cursor->proximo = proximo;
+    }
+}
+
+void imprimirLista(Nodo *lista){
+    if(lista == NULL){
+        printf("La lista esta vacia\n");
+    }else{
+        Nodo *cursor = lista;
+        while (cursor != NULL) {
+            printf("%d\n", cursor->valor);
+            cursor = cursor->proximo;
+        }
+    }
+}
+
+int main() {
+
+    Nodo *lista = crearLista();
+    agregarElemento(lista, 1);
+    imprimirLista(lista);
+   /* agregarElemento(lista, 2);
+    imprimirLista(lista);
+    lista = agregarElemento(lista, 3);
+    imprimirLista(lista);
+    lista = agregarElemento(lista, 4);
+    imprimirLista(lista);
+    lista = agregarElemento(lista, 5);
+    
+    imprimirLista(lista);*/
+
+}
